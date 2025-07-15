a377139d49c112ee65da5af4a31ac8a835187018
benrbowers <benjambowers@gmail.com>
2025-07-15 09:50:03 -0400

    ✅ CH1 L3 passed

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	input, err := os.Open("messages.txt")
+	if err != nil {
+		panic("Could not open messages.txt:\n" + err.Error())
+	}
+
+	word := make([]byte, 8)
+	for {
+		n, err := input.Read(word)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		if n > 0 {
+			fmt.Printf("read: %s\n", string(word[0:n]))
+		}
+	}
 }
